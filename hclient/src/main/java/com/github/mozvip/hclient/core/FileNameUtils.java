67d05e35805cd894bed6bcbb315788b4016590bd
Guillaume Serre <guillaume.serre@gmail.com>
2017-01-08 18:06:14 +0100

    Fix file encoding

--- hclient/src/main/java/com/github/mozvip/hclient/core/FileNameUtils.java
+++ hclient/src/main/java/com/github/mozvip/hclient/core/FileNameUtils.java
@@ -3,8 +3,8 @@ package com.github.mozvip.hclient.core;
 public class FileNameUtils {
 	
 	public static String sanitizeFileName( String fileName ) {
-		String cleanFileName = fileName.replace('?', '¿');
-		cleanFileName = cleanFileName.replace('é', 'e');	// FIXME : replace all accents 
+		String cleanFileName = fileName.replace('?', 'Â¿');
+		cleanFileName = cleanFileName.replace('Ã©', 'e');	// FIXME : replace all accents 
 		cleanFileName = cleanFileName.replaceAll("[\\|\\:\\*/\"]", " ");
 		cleanFileName = cleanFileName.replaceAll("\\s+", " ").trim();
 		while (cleanFileName.endsWith(".")) {
